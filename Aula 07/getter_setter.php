319dfe509fa618992efc9c5949a799651895bcd0
GabrielXavier125 <gx3848@gmail.com>
2025-08-27 16:15:15 -0300

    27.08.2025

--- Aula 07/getter_setter.php
+++ Aula 07/getter_setter.php
@@ -18,4 +18,33 @@ class Pessoa {
         $this->email = $email;
         $this->senha = $senha;
     }
+
+    // Getter e Setter para $nome
+    public function setNome($nome) {  // Setter Nome
+        $this->nome = ucwords(strtolower($nome));
+    }
+
+    public function getNome() {  // Getter Nome
+        return $this->nome;
+    }
+
+    // Getter e Setter para $cpf
+    public function setCpf($cpf) {  // Setter CPF
+        $this->cpf = preg_replace('/\D/', '', $cpf); // Remove caracteres não numéricos
+    }
+
+    public function getCpf() {  // Getter CPF
+        return $this->cpf;
+    }
+
+    public function setTelefone($telefone) {  // Setter Telefone
+        $this->telefone = preg_replace('/[^0-9]/', '', $telefone); // Remove caracteres não numéricos
+    }
+
+    public function getTelefone() {  // Getter Telefone
+        return $this->telefone; 
+    }
+
+    
+    
 }
